c8ed52099392a2ff7c31669d50492fa54bdc835e
Sesostris Vieira <sesostrisvieira@gmail.com>
2024-08-12 16:03:10 -0300

    Adiciona search_url, separa theme toggler, e outros pequenos ajustes

--- global_djbs_settings.py
+++ global_djbs_settings.py
@@ -4,11 +4,13 @@ from djbs.djbs_constants import *
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DJBSTHEME_DEFAULTS = {
+    "SEARCH_URL": None,
+    "SEARCH_PARAM": None,
     "MENU_FILE": BASE_DIR / "menu_conf.yaml",
     "CHECK_AS_SWITCH": True,
-    "FILTER_STYLE": FILTER_STYLE_FORM,
-    "FIELDSET_STYLE": STYLE_TAB,
-    "INLINESET_STYLE": STYLE_TAB,
+    "FILTER_STYLE": FILTER_STYLE_CLASSIC,
+    "FIELDSET_STYLE": STYLE_CARD,
+    "INLINESET_STYLE": STYLE_CARD,
     "BADGERIZE_FACETS": True,
     "TOOL_ICONS": {
         "add": "bi bi-plus-square",
