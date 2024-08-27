80f078c45e8d4691984e640e35e7130a69410789
moonshinerd <vitorhls15@gmail.com>
2024-08-26 21:20:01 -0300

    fix: voltando a configuração para evitar confitos de merge

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from .global_djbs_settings import DJBSTHEME_DEFAULTS
+from djbs.global_djbs_settings import DJBSTHEME_DEFAULTS
 
 if hasattr(settings, "DJBSTHEME"):
     for key, value in settings.DJBSTHEME.items():
