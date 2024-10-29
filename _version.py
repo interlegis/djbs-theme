f21c416192dae867fc0e886ffcd1f472579f4bd6
moonshinerd <vitorhls15@gmail.com>
2024-10-29 19:35:07 -0300

    fix: retornando related_widget_wrapper e base.html ao modelo original e excluinda argumentos class desnecessarios

--- _version.py
+++ _version.py
@@ -3,7 +3,6 @@
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from typing import Tuple, Union
-
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
@@ -13,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.1.dev0+gace9d0d.d20241003"
-__version_tuple__ = version_tuple = (0, 1, 1, "dev0", "gace9d0d.d20241003")
+__version__ = version = '0.1.dev50+gcc2f16c.d20241029'
+__version_tuple__ = version_tuple = (0, 1, 'dev50', 'gcc2f16c.d20241029')
