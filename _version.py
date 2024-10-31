9ee126b5068ff8e43e5400b9e67f0429e971f063
moonshinerd <vitorhls15@gmail.com>
2024-10-31 15:00:37 -0300

    fix: consertando o código para atualizar o estilo de todos os duallistbox presentes

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev52+gef6bd27.d20241031'
-__version_tuple__ = version_tuple = (0, 1, 'dev52', 'gef6bd27.d20241031')
+__version__ = version = '0.1.dev53+gd6595cc.d20241031'
+__version_tuple__ = version_tuple = (0, 1, 'dev53', 'gd6595cc.d20241031')
