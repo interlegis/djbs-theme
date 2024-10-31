ef6bd2704b49615906ad1e82f037a6b38f79ebc6
moonshinerd <vitorhls15@gmail.com>
2024-10-30 21:45:57 -0300

    feat: estido do dualistbox em bootstrap implementado com javascript e sem uso de bibliotecas externas

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev50+gcc2f16c.d20241029'
-__version_tuple__ = version_tuple = (0, 1, 'dev50', 'gcc2f16c.d20241029')
+__version__ = version = '0.1.dev51+gf21c416.d20241031'
+__version_tuple__ = version_tuple = (0, 1, 'dev51', 'gf21c416.d20241031')
