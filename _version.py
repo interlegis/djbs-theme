07150c245c054b7e5e08bdad8dcb625c67c8d317
Sesóstris Vieira <sesostrisvieira@gmail.com>
2024-12-03 12:52:13 -0300

    Ajusta templates de geração de forms e outros widgets

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev45+g038d130.d20241105'
-__version_tuple__ = version_tuple = (0, 1, 'dev45', 'g038d130.d20241105')
+__version__ = version = '0.0.1'
+__version_tuple__ = version_tuple = (0, 0, 1)
