c4af31b19b5663884812f360641426b975ac5459
Sesóstris Vieira <sesostrisvieira@gmail.com>
2025-03-28 12:32:55 -0300

    Add version stamp in package code

--- src/djbs/__init__.py
+++ src/djbs/__init__.py
@@ -1,3 +1,24 @@
+VERSION = (1, 0, 0, "DEV")
+
+
+def get_version(version):
+    """Dynamically calculate the version based on VERSION tuple."""
+    if len(version) > 2 and version[2] is not None:
+        if len(version) == 4:
+            str_version = "%s.%s.%s.%s" % version
+        elif isinstance(version[2], int):
+            str_version = "%s.%s.%s" % version[:3]
+        else:
+            str_version = "%s.%s_%s" % version[:3]
+    else:
+        str_version = "%s.%s" % version[:2]
+
+    return str_version
+
+
+__version__ = get_version(VERSION)
+
+
 def get_djbst_settings():
     from django.conf import settings
     from .global_djbs_settings import DJBSTHEME_DEFAULTS
